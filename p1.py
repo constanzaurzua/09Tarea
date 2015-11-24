b8cac7417bc9064e860558460141e1e0387d62a4
constanzaurzua <urzuacisterna@gmail.com>
2015-11-24 01:22:42 -0300

    p1 lista

--- p1.py
+++ p1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 from scipy.optimize import (leastsq, curve_fit)
 
 ''' calcular la cte de hubble con un intervalor de confianza de 95%'''
@@ -12,10 +12,10 @@ def funcion_modelo(parametro,D):
     return H*D
 
 
-def funcion_modelo2(parametro2,v):
+def funcion_modelo2(parametro,v):
     '''otra forma de escrbir la ecuacion'''
-    H = parametro2
-    return v/D
+    H = parametro
+    return v/H
 
 
 def funcion_min(D,H):
@@ -24,13 +24,76 @@ def funcion_min(D,H):
     return funcion_modelo(parametro, D)
 
 
-def funcion_min2(D, v):
-    paramtro2 = H
-    return funcion_modelo2(parametro2, v)
+def funcion_min2(v,H):
+    '''minimiza la funcion 2'''
+    parametro = H
+    return funcion_modelo2(parametro, v)
 
-datos=np.loadtxt('hubble_original.dat')
-parametro =  datos[:,0]
-parametro2 = datos[:,1]
 
-resultado = leastsq(func_min, parametro, args=(x_muestra, y_muestra, y_scales))
-resultado2 = leastsq(func_min2, parametro2, args=(x_muestra, y_muestra, y_scales))
+def bootstrap(datos):
+    '''funcion que calculara errores'''
+
+    N = datos.shape[0]
+    N1 = datos.shape[1]
+    Nboot = 10000
+    mean_values = np.zeros(Nboot)
+
+    for i in range(Nboot):
+        s = np.random.randint(low=0, high=N, size=N)
+        datos_f = datos[s][s]
+        distancia = datos_f[:,0]
+        velocidad = datos_f[:,1]
+        a_optimo, a_covarianza = curve_fit(funcion_min, distancia, velocidad, a_0)
+        a_optimo2, a_covarianza2 = curve_fit(funcion_min2, velocidad, distancia, a_0)
+        a_promedio = (a_optimo + a_optimo2) / 2
+        mean_values[i] = a_promedio
+    fig2, ax2 = plt.subplots()
+    plt.hist(mean_values, bins=30)
+    plt.axvline(valor_promedio, color='r')
+    ax2.set_title("Simulacion de bootstrap")
+    ax2.set_xlabel("H [Km/s /Mpc]")
+    ax2.set_ylabel("frecuencia")
+
+    mean_values = np.sort(mean_values)
+    limite_bajo = mean_values[int(Nboot * 0.025)]
+    limite_alto = mean_values[int(Nboot * 0.975)]
+    print "El intervalo de confianza al 95% es: [{}:{}]".format(limite_bajo, limite_alto)
+
+
+
+# main
+
+np.random.seed(5000)
+
+datos = np.loadtxt("hubble_original.dat")
+distancia =  datos[:,0]
+velocidad = datos[:,1]
+a_0 = 3
+a_optimo, a_covarianza = curve_fit(funcion_min, distancia, velocidad, a_0)
+a_optimo2, a_covarianza_2 = curve_fit(funcion_min2, velocidad, distancia,a_0)
+
+a_promedio = (a_optimo + a_optimo2) / 2
+valor_promedio = a_promedio
+print valor_promedio
+
+
+#plot
+
+fig = plt.figure()
+fig.clf()
+ax1 = fig.add_subplot(111)
+
+ax1.plot(distancia, velocidad, '*', label="Datos experimentales")
+ax1.plot(distancia, valor_promedio * distancia, label="Ajuste")
+
+ax1.set_xlim([-0.5, 2.5])
+ax1.set_xlabel("Distancia $[Mpc]$")
+ax1.set_ylabel("Velocidad $[km/s]$")
+ax1.set_title("Grafico de distancia $[Mpc]$ versus velocidad $[km/s]$")
+
+plt.legend(loc='upper left')
+plt.draw()
+plt.show()
+
+# Intervalo de confianza
+interv_confianza = bootstrap(datos)
