878eb91707069a7b26b63844ff65e09956952229
constanzaurzua <urzuacisterna@gmail.com>
2015-11-23 21:01:05 -0300

    funciones a minimizar

--- p1.py
+++ p1.py
@@ -10,7 +10,25 @@ def funcion_modelo(parametro,D):
     '''funcionar que minimizaremos'''
     H = parametro
     return H*D
-def funcion_modelo2(parametro2,D):
+
+
+def funcion_modelo2(parametro2,v):
     '''otra forma de escrbir la ecuacion'''
-    H = parametro2 
+    H = parametro2
     return v/D
+
+
+def funcion_min(parametro,xdata, ydata,yerror):
+    ''' minimizacion de la funcion '''
+    return (ydata - funcion_modelo(parametro, xdata)) / yerrors
+
+
+def funcion_min2(parametro2, xdata, ydata, yerror):
+    return (ydata - funcion_modelo2(parametro2, xdata)) / yerrors
+
+datos=np.loadtxt('hubble_original.dat')
+parametro =  datos[:,0]
+parametro2 = datos[:,1]
+
+resultado = leastsq(func_min, parametro, args=(x_muestra, y_muestra, y_scales))
+resultado2 = leastsq(func_min2, parametro2, args=(x_muestra, y_muestra, y_scales))
